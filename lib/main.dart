59c9031be262f92aefecd53e3eee2990c677e369
Felipe Nobre <nobrefelipe1@gmail.com>
2020-02-17 18:13:02 +0000

    removed irrelevant  comments

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,4 @@ import 'package:flutter/material.dart';
 import 'package:weddy/app/app_module.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
-/*
-  InjectShared Preferecnes plugin
-  so we can save data to the device's memory
-  eg: the user token after authentication, so we can keep the user authenticated
-*/
 void main() => runApp(ModularApp(module: AppModule()));
